e3af757aee680abb6492aec6897ed16af3801ebc
nwm-54 <vi_tran@berkeley.edu>
2023-03-26 13:41:57 -0700

    add metrics for argo_events and argo_workflows

--- apps/argo_workflows.py
+++ apps/argo_workflows.py
@@ -12,4 +12,12 @@ def objects() -> Generator[dict, None, None]:
     yield from helm.build_chart_from_versions(
         name="argo-workflows",
         versions=get_versions(__file__),
+        values={
+            "controller": {
+                "metrics": {
+                    "enabled": True, 
+                    "serviceMonitor": {"enabled": True},
+                },
+            },
+        },
     )
